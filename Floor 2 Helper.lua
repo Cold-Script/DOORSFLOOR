4207a41b1ececab5347c62773c3c94e4961eac30
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-02 09:22:09 +0700

    Update Floor 2 Helper.lua

--- Floor 2 Helper.lua
+++ Floor 2 Helper.lua
@@ -29,7 +29,10 @@ local Group = {
   Left1 = Tabs.Main:AddLeftGroupbox('GUI'),
   Right1 = Tabs.Main:AddRightGroupbox('Credits'),
   Left2 = Tabs.Cheat:AddLeftGroupbox('Players'),
-  Right2 = Tabs.Cheat:AddRightGroupbox('Misc Player')
+  Left3 = Tabs.Visual:AddLeftGroupbox("Disabled"),
+  Right2 = Tabs.Visual:AddRightGroupbox("Anti"),
+  Left4 = Tabs.Visual:AddLeftGroupbox("Camera"),
+  Right3 = Tabs.Visual:AddRightGroupbox("ESP')
 }
 Group.Left1:AddButton({
     Text = "Unload Library (Kill GUI)",
@@ -42,51 +45,32 @@ end})
 Group.Right1:AddLabel("Credits by rechedmcvn")
 Group.Left2:AddSlider('',{
     Text = "Speed Boost",
-    Default = 25,
-    Min = 16,
-    Max = 25,
+    Default = 1,
+    Min = 1,
+    Max = 6,
     Rounding = 1,
     Compact = true,
-    Callback = function(v)
-        values.speedV = v
+    Callback = function(Speed)
+    game.Players.LocalPlayer.Character.Humanoid:SetAttribute('SpeedBoostBehind', Speed)    
     end
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-PlayerTab = PlayerTab:AddLeftGroupbox("ESP")
-PlayerTab:AddToggle("",{Text="Doors ESP",Callback=function(V1)
-if V1 then
-for _,v in pairs(workspace:GetDescendants()) do
-if v.Name == "Door" and v.Parent.Name == "Door" then
-Highlight(v, "Door", Color3.fromRGB(80,255,200), "DoorESP")
-end
-end
-ESP1 = workspace.CurrentRooms.ChildAdded:Connect(function(child)
-for _,v in pairs(workspace:GetDescendants()) do
-if v.Name == "Door" and v.Parent.Name == "Door" then
-Highlight(v, "Door", Color3.fromRGB(80,255,200), "DoorESP")
-end
-end
-end)
-else
-ESP1:Disconnect()
-for _, v in pairs(workspace:GetDescendants()) do
-if v.Name == "DoorESP" then
-v:Destroy()
-end
-end
-end
+Group.Left2:AddButton({
+        Text = "Reset Character",
+        DoubleClick = true,
+        Func = function()
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
 end})
+
+game:GetService("RunService").RenderStepped:Connect(function()pcall(function()if _G.SlowDownnnonononoo then if (game.Players.LocalPlayer.Character.Head.Massless==true) then local v451=1726 -(1668 + 58) ;local v452;while true do if (v451==(626 -(512 + 114))) then v452=false;game.Players.LocalPlayer.Character.Head.Massless=v452;game.Players.LocalPlayer.Character.LeftFoot.Massless=v452;game.Players.LocalPlayer.Character.LeftHand.Massless=v452;v451=2 -1 ;end if ((1 -0)==v451) then game.Players.LocalPlayer.Character.LeftLowerArm.Massless=v452;game.Players.LocalPlayer.Character.LeftLowerLeg.Massless=v452;game.Players.LocalPlayer.Character.LeftUpperArm.Massless=v452;game.Players.LocalPlayer.Character.LeftUpperLeg.Massless=v452;v451=6 -4 ;end if (v451==(2 + 1)) then game.Players.LocalPlayer.Character.RightLowerLeg.Massless=v452;game.Players.LocalPlayer.Character.RightUpperArm.Massless=v452;game.Players.LocalPlayer.Character.RightUpperLeg.Massless=v452;game.Players.LocalPlayer.Character.UpperTorso.Massless=v452;break;end if (v451==2) then game.Players.LocalPlayer.Character.LowerTorso.Massless=v452;game.Players.LocalPlayer.Character.RightFoot.Massless=v452;game.Players.LocalPlayer.Character.RightHand.Massless=v452;game.Players.LocalPlayer.Character.RightLowerArm.Massless=v452;v451=3;end end end end end);end);Group.Left2:AddToggle("",{Text="No Slow",Default=false,Tooltip="No Slow Down",Callback=function(v85)_G.SlowDownnnonononoo=v85;if (_G.SlowDownnnonononoo==false) then local v245=true;game.Players.LocalPlayer.Character.Head.Massless=v245;game.Players.LocalPlayer.Character.LeftFoot.Massless=v245;game.Players.LocalPlayer.Character.LeftHand.Massless=v245;game.Players.LocalPlayer.Character.LeftLowerArm.Massless=v245;game.Players.LocalPlayer.Character.LeftLowerLeg.Massless=v245;game.Players.LocalPlayer.Character.LeftUpperArm.Massless=v245;game.Players.LocalPlayer.Character.LeftUpperLeg.Massless=v245;game.Players.LocalPlayer.Character.LowerTorso.Massless=v245;game.Players.LocalPlayer.Character.RightFoot.Massless=v245;game.Players.LocalPlayer.Character.RightHand.Massless=v245;game.Players.LocalPlayer.Character.RightLowerArm.Massless=v245;game.Players.LocalPlayer.Character.RightLowerLeg.Massless=v245;game.Players.LocalPlayer.Character.RightUpperArm.Massless=v245;game.Players.LocalPlayer.Character.RightUpperLeg.Massless=v245;game.Players.LocalPlayer.Character.UpperTorso.Massless=v245;end end});
+game.Players.LocalPlayer.Character.Humanoid:GetPropertyChangedSignal("MoveDirection"):Connect(function()if (_G.ClosetExitFix and (game.Players.LocalPlayer.Character:GetAttribute("Hiding")==true)) then game:GetService("ReplicatedStorage").EntityInfo.CamLock:FireServer();end end);Group.Left2:AddToggle("",{Text="Closet Exit Fixed",Default=false,Tooltip="Closet Exit Fast",Callback=function(v104)_G.ClosetExitFix=v104;end})    
+Group.Left3:AddToggle('',{
+            Text = "Disabled Void ROBLOX",
+            Callback = function(value)
+if value then
+workspace.FallenPartsDestroyHeight = 0 / 0
+        else
+            workspace.FallenPartsDestroyHeight = OldVoid
+                end
+            end})
+game:GetService("RunService").RenderStepped:Connect(function()pcall(function()if _G.MSHNL then if game.workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Assets"):FindFirstChild("Chandelier") then game.workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Assets").Chandelier:Destroy();end end end);end);game:GetService("RunService").RenderStepped:Connect(function()pcall(function()if _G.MSHNL then if game.workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Assets"):FindFirstChild("Light_Fixtures") then game.workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Assets").Light_Fixtures:Destroy();end end end);end)
+Group.Left3:AddToggle("",{Text = "Disabled Light",Default = false,Tooltip = "No Light",Callback = function(NL)_G.MSHNL = NL ;end})  
