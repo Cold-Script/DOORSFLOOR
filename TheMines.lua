4f6404b0aaf96e462211c0fd4ed517d8a417bf01
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-09-05 10:51:42 +0700

    Update TheMines.lua

--- TheMines.lua
+++ TheMines.lua
@@ -28,14 +28,24 @@ local function Billboard(child, name, color, title)
     uistroke.Thickness = 1
     uistroke.Parent = Title
 task.spawn(function()
-  repeat
+while wait() do
     Title.Text = string.format('%s\n[ %s ]', (name or child.Name), math.floor((workspace.CurrentCamera.CFrame.Position - child:GetPivot().Position).Magnitude))
 game:GetService('RunService').RenderStepped:Wait()
-            until Title.Parent = Billboard
+            Title.Parent = Billboard
+              end
         end)
 end
 
-local function Billboard(child, name, color, title)
+
+
+local rep = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/'
+local lib =   loadstring(game:HttpGet(rep.. 'Library.lua'))()
+local save =  loadstring(game:HttpGet(rep.. 'addons/SaveManager.lua'))()
+local theme = loadstring(game:HttpGet(rep.. 'addons/ThemeManager.lua'))()
+
+local Options = getgenv().Options
+
+local Window = local function Billboard(child, name, color, title)
     local Billboard = Instance.new("BillboardGui")
     Billboard.Active = true
     Billboard.AlwaysOnTop = true
@@ -60,23 +70,15 @@ local function Billboard(child, name, color, title)
     uistroke.Thickness = 1
     uistroke.Parent = Title
 task.spawn(function()
-  repeat
+while wait() do
     Title.Text = string.format('%s\n[ %s ]', (name or child.Name), math.floor((workspace.CurrentCamera.CFrame.Position - child:GetPivot().Position).Magnitude))
 game:GetService('RunService').RenderStepped:Wait()
-            until Title.Parent = Billboard
+            Title.Parent = Billboard
+              end
         end)
 end
-
-
-local rep = 'https://raw.githubusercontent.com/mstudio45/LinoriaLib/main/'
-local lib =   loadstring(game:HttpGet(rep.. 'Library.lua'))()
-local save =  loadstring(game:HttpGet(rep.. 'addons/SaveManager.lua'))()
-local theme = loadstring(game:HttpGet(rep.. 'addons/ThemeManager.lua'))()
-
-local Options = getgenv().Options
-
-local Window = lib:CreateWindow({
-    Title = '(YOU HUB) Script Floor 2 Helper | v1.0',
+lib:CreateWindow({
+    Title = '(YOU HUB) Floor 2 Helper | v1.0',
     Center = true,
     AutoShow = true,
     TabPadding = 8,
